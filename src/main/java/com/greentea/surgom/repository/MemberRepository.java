0216c2dcd3fd4c39b28f4a0903c997b32a4f8633
moong2 <www.castlehi@gmail.com>
2022-10-13 17:18:19 +0900

    FEAT : jpaRepository 도입

--- src/main/java/com/greentea/surgom/repository/MemberRepository.java
+++ src/main/java/com/greentea/surgom/repository/MemberRepository.java
@@ -1,18 +1,27 @@
 package com.greentea.surgom.repository;
 
+import com.greentea.surgom.domain.Gender;
 import com.greentea.surgom.domain.Member;
+import org.hibernate.annotations.SQLUpdate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface MemberRepository {
-    String save(Member member);
-    Member findByPhone(String phone);
-    Member findByPhoneAndIdentifier(String phone, String identifier);
-    void delete(String phone);
-    void deleteAll();
-    List<Member> findAll();
-    List<Member> findAllWithAge_range(int first, int last);
-    List<Member> findAllWithGender(String gender);
-    List<Member> findAllWithAgeAndGender(int first, int last, String gender);
-    void update(Member member);
+@Repository
+@Transactional
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByPhone(String phone);
+    Optional<Member> findByPhoneAndIdentifier(String phone, String identifier);
+    List<Member> findByAgeBetween(int first, int last);
+    List<Member> findByGender(Gender gender);
+    List<Member> findByAgeBetweenAndGender(int first, int last, Gender gender);
+    @Query("update Member m set m.phone = :phone where m.identifier = :identifier")
+    @Modifying
+    void updatePhone(@Param("phone") String phone, @Param("identifier") String identifier);
 }

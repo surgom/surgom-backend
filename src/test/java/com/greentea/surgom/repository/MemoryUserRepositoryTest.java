74653f27502fa4c0182a6a1663acac127c9b2f0d
moong2 <www.castlehi@gmail.com>
2022-10-17 15:56:35 +0900

    FEAT : 출생년도 저장으로 변경

--- src/test/java/com/greentea/surgom/repository/MemoryUserRepositoryTest.java
+++ src/test/java/com/greentea/surgom/repository/MemoryUserRepositoryTest.java
@@ -131,17 +131,17 @@ public class MemoryUserRepositoryTest {
         Member member = new Member();
         member.setName("박성하");
         member.setPhone("010-4172-8563");
-        member.setAge(23);
+        member.setAge(2000);
 
         Member member2 = new Member();
         member2.setName("한보은");
         member2.setPhone("010-1234-5678");
-        member2.setAge(100);
+        member2.setAge(1914);
 
         Member member3 = new Member();
         member3.setName("한보은");
         member3.setPhone("010-9876-5432");
-        member3.setAge(23);
+        member3.setAge(2000);
 
         //when
         memberRepository.save(member);
@@ -149,10 +149,10 @@ public class MemoryUserRepositoryTest {
         memberRepository.save(member3);
 
         //then
-        List<Member> list = memberRepository.findByAgeBetween(20, 29);
+        List<Member> list = memberRepository.findByAgeBetween(1994, 2000);
         assertEquals(list.size(), 2);
 
-        List<Member> list2 = memberRepository.findByAgeBetween(100, 109);
+        List<Member> list2 = memberRepository.findByAgeBetween(1914, 1923);
         assertEquals(list2.size(), 1);
         assertEquals(list2.get(0).getName(), "한보은");
     }
@@ -195,19 +195,19 @@ public class MemoryUserRepositoryTest {
         Member member = new Member();
         member.setName("박성하");
         member.setPhone("010-4172-8563");
-        member.setAge(23);
+        member.setAge(2000);
         member.setGender(Gender.FEMALE);
 
         Member member2 = new Member();
         member2.setName("박성하");
         member2.setPhone("010-1234-5678");
-        member2.setAge(23);
+        member2.setAge(2000);
         member2.setGender(Gender.MALE);
 
         Member member3 = new Member();
         member3.setName("한보은");
         member3.setPhone("010-9876-5432");
-        member3.setAge(100);
+        member3.setAge(1914);
         member3.setGender(Gender.FEMALE);
 
         //when
@@ -216,11 +216,11 @@ public class MemoryUserRepositoryTest {
         memberRepository.save(member3);
 
         //then
-        List<Member> list = memberRepository.findByAgeBetweenAndGender(20, 29, Gender.FEMALE);
+        List<Member> list = memberRepository.findByAgeBetweenAndGender(1994, 2000, Gender.FEMALE);
         assertEquals(list.size(), 1);
         assertEquals(list.get(0).getPhone(), "010-4172-8563");
 
-        List<Member> list2 = memberRepository.findByAgeBetweenAndGender(20, 29, Gender.MALE);
+        List<Member> list2 = memberRepository.findByAgeBetweenAndGender(1994, 2000, Gender.MALE);
         assertEquals(list2.size(), 1);
         assertEquals(list2.get(0).getPhone(), "010-1234-5678");
     }
